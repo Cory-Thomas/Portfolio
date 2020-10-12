431ace85eee6d5b0038da6b091468934ad8bf6ed
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-12 19:57:19 -0400

    added backgrounds, box shadows, a color scheme, font-size, and borders between each section

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -5,14 +5,20 @@ import Projects from './Projects.js';
 import About from './About.js';
 import Contact from './Contact.js'; 
 import './App.css';
+import { SportsRugbySharp } from '@material-ui/icons';
 
 const App = () => {
   return (
       <> 
         <Navbar />
         <HeroSection />
+        
+          <div className='section-line'></div >
+          
         <Projects />
+        <div className='section-line' >About Me</div>
         <About />
+        <div className='section-line'>Contact</div>
         <Contact />
       </>
   );
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { Link } from 'react-scroll';
 import Slide from 'react-reveal/Slide';
+import Fade from 'react-reveal/Fade';
  
 const Projects = () => {
   return (
     <Slide down >
     <div className='navbar'>
-        <Slide down delay={500}>
-            <div>Cory Thomas</div>
-        </Slide>
-        <Slide down delay={500}>
+        <Fade down delay={500}>
+            <div className='nav-name'>Cory Thomas</div>
+        </Fade>
+        {/* <Slide down delay={500}> */}
         <div className='nav-items'>
+        
             <Link 
             activeClass='active'
             to='hero-section'
@@ -18,9 +20,10 @@ const Projects = () => {
             smooth={ true }
             offset={ 0 }
             duration={ 1000 }
-            >
-                Top
+            ><Fade down delay={500}>
+                <p>Top</p></Fade>
             </Link>
+            
             <Link 
                 activeClass='active'
                 to='projects-section'
@@ -28,9 +31,10 @@ const Projects = () => {
                 smooth={ true }
                 offset={ 0 }
                 duration={ 1000 }
-            >
-                Projects
+            ><Fade down delay={800}>
+                Projects</Fade>
             </Link>
+            
             <Link 
                 activeClass='active'
                 to='about-section'
@@ -38,9 +42,10 @@ const Projects = () => {
                 smooth={ true }
                 offset={ 0 }
                 duration={ 1000 }
-            >
-                About Me
+            ><Fade down delay={1100}>
+                About Me</Fade>
             </Link>
+            
             <Link 
                 activeClass='active'
                 to='contact-section'
@@ -48,11 +53,11 @@ const Projects = () => {
                 smooth={ true }
                 offset={ 0 }
                 duration={ 1000 }
-            >
-                Contact
+            ><Fade down delay={1400}>
+                Contact</Fade>
             </Link>
         </div>
-        </Slide>
+        {/* </Slide> */}
     </div>
     </Slide>
   )
