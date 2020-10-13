892b623139de07aa513d3cd65038c70a1e224901
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-13 15:57:18 -0400

    added an animation to the hero section about my details

--- portfolio/src/components/HeroSection.js
+++ portfolio/src/components/HeroSection.js
@@ -2,11 +2,33 @@ import React from "react";
  
 const HeroSection = () => {
   return (
-      <div className="hero-section">
-          <h1>Cory Thomas</h1>
-          <h2>Web Dev</h2>
-          <div>Skill icons?</div>
+    <div className="hero-section">
+      <div className='hero-content'>
+        <div>
+          <h2>Hi, my name is</h2>
         </div>
+        <div className='hero-name'>
+          <h1>Cory Thomas.</h1>
+        </div>
+        <div>
+          <h2>
+            I'm a 
+            <span className='hero-word'> Web Developer </span>
+             and &nbsp; 
+              <span className='word hero-word'>
+                <span>Pianist</span>
+                <span>Bonsai Enthusiast</span>
+                <span>Gamer</span>
+                <span>Cat Dad</span>
+              </span>
+             
+             {/* <span className='hero-word'> multipotentialite </span>
+              (many interests). */}
+            </h2>
+        </div>
+        {/* <div>Skill icons?</div> */}
+      </div>
+    </div>
   )
 };
  
