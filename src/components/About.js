30f4d589b4a87fb21f0ec0dd8f051f7d6a591101
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-15 18:21:48 -0400

    added content and some styling for the about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,15 +3,35 @@ import React from "react";
 const About = () => {
   return (
     <div className="about-section">
-      <section>
-        <div>About Me</div>
+      <section className='about-wrapper'>
         <div>
           <p>Hello! My name is Cory Thomas, a web developer based in Chattanooga, Tennessee.</p>
           <p></p>
         </div>
+        <div className='skills-wrapper'>
+          <div>Skills</div>
+          <section className='about-skills'>
+            <div>
+              <div>JavaScript</div>
+              <div>HTML5</div>
+              <div>CSS3</div>
+              <div>Sass</div>
+            </div>
+            <div>
+              <div>React</div>
+              <div>React Hooks</div>
+              <div>Redux</div>
+              <div>Context API</div>
+            </div>
+            <div>
+              <div>Jest</div>
+              <div>Cypress</div>
+              <div>React Testing Library</div>
+              <div>Git/GitHub</div>
+            </div>
+          </section>
+        </div>
       </section>
-      <div>Contact Me</div>
-      <div>Mail Icon or Form?</div>
     </div>
   )
 };
