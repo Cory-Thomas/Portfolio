81bd1183d1b16d5d67b4d9ee4a18c3035c4a7dd3
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-14 20:43:17 -0400

    styled form some

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,28 +3,33 @@ import React from "react";
 const Contact = () => {
   return (
     <div className="contact-section">
-        <form name="contact" method="post" data-netlify-recaptcha="true">
+      <div className='form-card'>
+        <form name="contact" method="post">
           <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>Name:&nbsp;
-              <input type="text" name="name"/>
+          <div className='input-container'>
+            <label>Name:
+              <div>
+                <input type="text" name="name"/>
+              </div>
             </label>
-          </p>
-          <p>
-            <label>Email:&nbsp;
-              <input type="email" name="email"/>
+          </div>
+          <div className='input-container'>
+            <label>Email:
+              <div>
+                <input type="email" name="email"/>
+              </div>
             </label>
-          </p>
-          <p>
-            <label>Message:&nbsp;
-              <textarea name="message"></textarea>
+          </div>
+          <div className='input-container'>
+            <label>Message:
+              <div>
+                <textarea name="message"></textarea>
+              </div>
             </label>
-          </p>
-          <div data-netlify-recaptcha="true"></div>
-          <p>
+          </div>
             <button type="submit">Send</button>
-          </p>
         </form>
+      </div>
     </div>
   )
 };
