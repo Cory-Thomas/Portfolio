e502fb8d9076419b91171c5e8e31f0a39bcd0d9e
Cory Thomas <cory.a.thomas1992@gmail.com>
2021-02-25 19:48:44 -0500

    updated favicon and page title

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -6,7 +6,7 @@ var plugins = [{
       options: {"plugins":[],"fonts":["Mulish","sans-serif:300","400","500","600","700"],"display":"swap"},
     },{
       plugin: require('C:/Users/corya/Documents/Lambda/Portfolio/Portfolio/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/assets/images/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"901b09be0ecd561efea10c1b64e69e85"},
+      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/assets/images/favicon-32x32.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"b76a0f2e3f4930b20f53cf4f4df4237d"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
